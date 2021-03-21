ca21dbce191ff22d2d67103d5ddc243b386938d6
xoffmod <79679205+xoffmod@users.noreply.github.com>
2021-03-21 08:39:07 +0100

    Add project files

---
+++ Main.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using UnityEngine;
+using UnityModManagerNet;
+
+namespace WeaponFocusPlus
+{
+    public class Settings : UnityModManager.ModSettings
+    {
+        public int FeatFormatIndex = 0;
+        public int WeaponCategoryFormatIndex = 0;
+
+        public override void Save(UnityModManager.ModEntry modEntry)
+        {
+            Save(this, modEntry);
+        }
+    }
+
+#if DEBUG
+    [EnableReloading]
+#endif
+    public class Main
+    {
+        public static bool Enabled = true;
+        public static Settings Settings = new Settings();
+        public static UnityModManager.ModEntry.ModLogger Logger;
+        public static HarmonyLib.Harmony HarmonyInstance;
+
+        public static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            Logger = modEntry.Logger;
+
+            Settings = Settings.Load<Settings>(modEntry);
+            modEntry.OnGUI = new Action<UnityModManager.ModEntry>(Main.OnGUI);
+            modEntry.OnSaveGUI = new Action<UnityModManager.ModEntry>(Main.OnSaveGUI);
+            modEntry.OnToggle = new Func<UnityModManager.ModEntry, bool, bool>(Main.OnToggle);
+            modEntry.OnUnload = new Func<UnityModManager.ModEntry, bool>(Main.OnUnload);
+
+            WeaponFocus.Init(modEntry.Path);
+
+            HarmonyInstance = new HarmonyLib.Harmony(modEntry.Info.Id);
+            HarmonyInstance.PatchAll(typeof(Main).Assembly);
+
+            return true;
+        }
+
+        static bool OnUnload(UnityModManager.ModEntry modEntry)
+        {
+#if DEBUG
+            HarmonyInstance.UnpatchAll(null);
+#endif
+            return true;
+        }
+
+        public static bool OnToggle(UnityModManager.ModEntry modEntry, bool value)
+        {
+            Enabled = value;
+            return true;
+        }
+
+        public static void OnGUI(UnityModManager.ModEntry modEntry)
+        {
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Show group in feat name: ", GUILayout.ExpandWidth(false));
+            Settings.FeatFormatIndex = GUILayout.SelectionGrid(Settings.FeatFormatIndex, WeaponFocus.WeaponGroupFormatSettings, WeaponFocus.WeaponGroupFormatSettings.Length, GUILayout.ExpandWidth(false));
+            GUILayout.EndHorizontal();
+
+            GUILayout.BeginHorizontal();
+            GUILayout.Label("Show group in weapon category name: ", GUILayout.ExpandWidth(false));
+            Settings.WeaponCategoryFormatIndex = GUILayout.SelectionGrid(Settings.WeaponCategoryFormatIndex, WeaponFocus.WeaponGroupFormatSettings, WeaponFocus.WeaponGroupFormatSettings.Length, GUILayout.ExpandWidth(false));
+            GUILayout.EndHorizontal();
+        }
+
+        public static void OnSaveGUI(UnityModManager.ModEntry modEntry)
+        {
+            Settings.Save(modEntry);
+        }
+    }
+}
---
+++ WeaponFocus.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+
+namespace WeaponFocusPlus
+{
+    public static class WeaponFocus
+    {
+        public static void Init(string path)
+        {
+            WeaponGroupsMap = new Dictionary<string, string>();
+
+            var jDoc = JObject.Parse(System.IO.File.ReadAllText(path + "groups.json"));
+            foreach (var jGroup in jDoc.Children<JProperty>())
+            {
+                string groupName = jGroup.Name;
+                foreach (var weaponName in jGroup.Value.Values<string>())
+                {
+                    if (weaponName != null && weaponName != "")
+                    {
+                        WeaponGroupsMap.Add(weaponName, groupName);
+                    }
+                }
+            }
+        }
+
+        public static readonly string[] WeaponGroupFormatSettings = { "Off", "Name - Group", "Group - Name" };
+        private static readonly string[] WeaponGroupFormats = { null, "{0} - {1}", "{1} - {0}" };
+
+        private static Dictionary<string, string> WeaponGroupsMap { get; set; }
+
+        private static string GetWeaponGroup(Kingmaker.Enums.WeaponCategory weaponCategory)
+        {
+            return WeaponGroupsMap.TryGetValue(weaponCategory.ToString(), out var ret) ? ret : null;
+        }
+
+        private static string FormatWeaponGroupStr(Kingmaker.Enums.WeaponCategory weaponCategory, string format, string str)
+        {
+            if (str != null)
+            {
+                var wg = GetWeaponGroup(weaponCategory);
+                return wg != null ? String.Format(format, str, wg) : str;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(Kingmaker.Blueprints.Root.Strings.StatsStrings), "GetText")]
+        [HarmonyLib.HarmonyPatch(new Type[] { typeof(Kingmaker.Enums.WeaponCategory) })]
+        internal static class StatsStrings_GetText_Patch
+        {
+            internal static void Postfix(Kingmaker.Enums.WeaponCategory stat, ref string __result)
+            {
+                if (Main.Enabled && Main.Settings.FeatFormatIndex != 0)
+                {
+                    __result = FormatWeaponGroupStr(stat, WeaponGroupFormats[Main.Settings.FeatFormatIndex], __result);
+                }
+            }
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(Kingmaker.Blueprints.Items.Weapons.BlueprintItemWeapon), "SubtypeName", HarmonyLib.MethodType.Getter)]
+        internal static class BlueprintItemWeapon_SubtypeName_Getter_Patch
+        {
+            internal static void Postfix(Kingmaker.Blueprints.Items.Weapons.BlueprintItemWeapon __instance, ref string __result)
+            {
+                if (Main.Enabled && Main.Settings.WeaponCategoryFormatIndex != 0)
+                {
+                    __result = FormatWeaponGroupStr(__instance.Category, WeaponGroupFormats[Main.Settings.WeaponCategoryFormatIndex], __result);
+                }
+            }
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(Kingmaker.UI.Common.UIUtilityItem), "GetHandUse")]
+        internal static class UIUtilityItem_GetHandUse_Patch
+        {
+            [HarmonyLib.HarmonyPriority(HarmonyLib.Priority.Last)]
+            internal static void Postfix(Kingmaker.Items.ItemEntity item, ref string __result)
+            {
+                if (Main.Enabled && Main.Settings.WeaponCategoryFormatIndex == 0 && (item is Kingmaker.Items.ItemEntityWeapon itemEntityWeapon))
+                {
+                    __result = FormatWeaponGroupStr(itemEntityWeapon.Blueprint.Category, "{0} ({1})", __result);
+                }
+            }
+        }
+
+        [HarmonyLib.HarmonyPatch(typeof(Kingmaker.Blueprints.Classes.Selection.FeatureParam), "Equals")]
+        [HarmonyLib.HarmonyPatch(new Type[] { typeof(Kingmaker.Blueprints.Classes.Selection.FeatureParam) })]
+        internal static class FeatureParam_Equals_Patch
+        {
+            internal static void Postfix(Kingmaker.Blueprints.Classes.Selection.FeatureParam __instance, Kingmaker.Blueprints.Classes.Selection.FeatureParam other, ref bool __result)
+            {
+                if (Main.Enabled && !__result && object.Equals(__instance.Blueprint, other?.Blueprint))
+                {
+                    if (__instance.SpellSchool.Equals(other?.SpellSchool) && __instance.StatType.Equals(other?.StatType))
+                    {
+                        if (__instance.WeaponCategory != null && other?.WeaponCategory != null)
+                        {
+                            var weaponGroup = GetWeaponGroup(__instance.WeaponCategory.Value);
+                            var weaponGroup2 = GetWeaponGroup(other.WeaponCategory.Value);
+
+                            if (weaponGroup != null && weaponGroup == weaponGroup2)
+                            {
+                                __result = true;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
